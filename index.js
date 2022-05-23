3d290d9ed03345c7e4426548ab3502348d1b2c44
Jazelle Pearce <pjazelle@yahoo.com>
2022-05-23 17:30:29 -0500

    files added

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquire");
+const fs = require("fs");
+const Employee = require("./libs/Employee")
+const Engineer = require("./libs/Engineer")
+const Intern = require("./libs/Intern")
+const Manager = require("./libs/Manager")
